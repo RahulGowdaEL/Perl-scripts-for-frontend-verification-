65311dbbea2ea13291b9098eb175f366f5e88171
Rahul Gowda <70683277+RahulGowdaEL@users.noreply.github.com>
2024-11-28 17:07:17 +0530

    Update bit_blast_wire.pl

--- bit_blast_wire.pl
+++ bit_blast_wire.pl
@@ -1,51 +1,45 @@
-#!/usr/bin/perl
 use strict;
 use warnings;
 
-# Input file paths
-my $file1 = 'file1.txt';  # File containing signal names to exclude
-my $file2 = 'file2.v';    # File to parse and modify
+# Input and output files
+my $file1 = 'file1.txt';
+my $file2 = 'file2.v';
+my $output_file = 'output.v';
 
-# Output file path
-my $output_file = 'output_file.v';
-
-# Hash to store signal names from File1 (with backslash)
+# Read excluded signal names from file1 into a hash
+open my $fh1, '<', $file1 or die "Cannot open $file1: $!";
 my %exclude_signals;
-
-# Read File1 and store signal names in a hash for quick lookup
-open my $fh1, '<', $file1 or die "[Err] : Cannot open $file1: $!";
-while (my $line = <$fh1>) {
-    chomp $line;
-    $exclude_signals{$line} = 1;  # Use signal name with backslash as a key
+while (<$fh1>) {
+    chomp;
+    s/^\s+|\s+$//g;    # Trim leading/trailing whitespace
+    $exclude_signals{$_} = 1;  # Store as keys in hash
 }
 close $fh1;
 
-# Process File2 and write to the output file
-open my $fh2, '<', $file2 or die "[Err] : Cannot open $file2: $!";
-open my $out, '>', $output_file or die "[Err] : Cannot open $output_file: $!";
+# Process file2
+open my $fh2, '<', $file2 or die "Cannot open $file2: $!";
+open my $out, '>', $output_file or die "Cannot open $output_file: $!";
 
 while (my $line = <$fh2>) {
-    chomp $line;
+    chomp($line);
 
-    # Check if the line matches any signal name in the exclusion list (File1)
-    if ($line =~ /\\([\w\[\]\*\_]+)/) {
-        my $signal_name = $1;
+    # Check for signals in the line
+    if ($line =~ /\\([\w\[\]\*\_]+)/) {  # Match backslash-prefixed signals
+        my $signal_name = "\\$1";  # Reconstruct signal name with backslash
 
-        # Skip modification for this line if the signal name is in the exclusion list
-        if (exists $exclude_signals{"\\$signal_name"}) {
-            print $out "$line\n";  # Write the line as-is
-            next;  # Skip the rest of the loop for this line
+        if (exists $exclude_signals{$signal_name}) {
+            # Do not modify this line
+            print $out "$line\n";
+            next;
         }
     }
 
-    # Remove backslashes from the line (for non-excluded signals)
+    # Remove backslashes from all other lines
     $line =~ s/\\//g;
-
-    # Write the modified line to the output file
     print $out "$line\n";
 }
 
 close $fh2;
 close $out;
 
-print "[Done] : Processed $file2 and created $output_file.\n";
+print "Processing complete. Output written to $output_file\n";
