6301a9b7d3cd39e24293c76b9e58c4c982d9472e
Rahul Gowda <70683277+RahulGowdaEL@users.noreply.github.com>
2024-11-28 16:36:04 +0530

    Update bit_blast_wire.pl

--- bit_blast_wire.pl
+++ bit_blast_wire.pl
@@ -9,14 +9,14 @@ my $file2 = 'file2.v';    # File to parse and modify
 # Output file path
 my $output_file = 'output_file.v';
 
-# Hash to store signal names from File1
+# Hash to store signal names from File1 (with backslash)
 my %exclude_signals;
 
 # Read File1 and store signal names in a hash for quick lookup
 open my $fh1, '<', $file1 or die "[Err] : Cannot open $file1: $!";
 while (my $line = <$fh1>) {
     chomp $line;
-    $exclude_signals{$line} = 1;  # Use signal name as a key
+    $exclude_signals{$line} = 1;  # Use signal name with backslash as a key
 }
 close $fh1;
 
@@ -28,13 +28,13 @@ while (my $line = <$fh2>) {
     chomp $line;
 
     # Check if the line matches any signal name in the exclusion list (File1)
-    if ($line =~ /\b\\([\w\[\]\*\_]+)\b/) {
+    if ($line =~ /\\([\w\[\]\*\_]+)/) {
         my $signal_name = $1;
 
-        # Skip processing if the signal name is in the exclusion list
+        # Skip modification for this line if the signal name is in the exclusion list
         if (exists $exclude_signals{"\\$signal_name"}) {
             print $out "$line\n";  # Write the line as-is
-            next;
+            next;  # Skip the rest of the loop for this line
         }
     }
 
