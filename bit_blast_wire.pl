c5fcc0b9387d97a09c8d04ad27ef3e703f78761a
Rahul Gowda <70683277+RahulGowdaEL@users.noreply.github.com>
2024-11-28 17:09:13 +0530

    Update bit_blast_wire.pl

--- bit_blast_wire.pl
+++ bit_blast_wire.pl
@@ -2,41 +2,44 @@ use strict;
 use warnings;
 
 # Input and output files
-my $file1 = 'file1.txt';
-my $file2 = 'file2.v';
-my $output_file = 'output.v';
+my $file1 = 'file1.txt';  # Contains excluded signal names
+my $file2 = 'file2.v';    # Input Verilog file
+my $output_file = 'output.v'; # Output Verilog file
 
-# Read excluded signal names from file1 into a hash
+# Read excluded signal names into a hash
 open my $fh1, '<', $file1 or die "Cannot open $file1: $!";
 my %exclude_signals;
 while (<$fh1>) {
     chomp;
-    s/^\s+|\s+$//g;    # Trim leading/trailing whitespace
-    $exclude_signals{$_} = 1;  # Store as keys in hash
+    s/^\s+|\s+$//g;    # Trim whitespace
+    $exclude_signals{$_} = 1;  # Store signal names as hash keys
 }
 close $fh1;
 
-# Process file2
+# Process the Verilog file
 open my $fh2, '<', $file2 or die "Cannot open $file2: $!";
 open my $out, '>', $output_file or die "Cannot open $output_file: $!";
 
 while (my $line = <$fh2>) {
     chomp($line);
 
-    # Check for signals in the line
-    if ($line =~ /\\([\w\[\]\*\_]+)/) {  # Match backslash-prefixed signals
-        my $signal_name = "\\$1";  # Reconstruct signal name with backslash
-
-        if (exists $exclude_signals{$signal_name}) {
-            # Do not modify this line
-            print $out "$line\n";
-            next;
+    # Check if the line contains any excluded signal
+    my $exclude_line = 0;
+    foreach my $excluded_signal (keys %exclude_signals) {
+        if ($line =~ /\Q$excluded_signal\E/) {  # Match exact signal name
+            $exclude_line = 1;
+            last;  # No need to check further, as this line should be excluded
         }
     }
 
-    # Remove backslashes from all other lines
-    $line =~ s/\\//g;
-    print $out "$line\n";
+    if ($exclude_line) {
+        # Leave the line unchanged
+        print $out "$line\n";
+    } else {
+        # Remove backslashes from all other lines
+        $line =~ s/\\//g;
+        print $out "$line\n";
+    }
 }
 
 close $fh2;
