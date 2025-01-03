c601e8564a016c6612cd646020c7a22a9fa9f3ab
Rahul Gowda <70683277+RahulGowdaEL@users.noreply.github.com>
2025-01-03 12:12:53 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -10,13 +10,8 @@ r0hd286_lnw3_tsmc_buft03p00 compile_PI_7562_229131 ( .a ( net_aps_213250 ) ,
     .z ( \TraceTargetNIU_Data[123] ) ) ;  
 
 
-r0hd117_lnw3_tsmc_buft03p00 compile_PI_7558_229127 ( .a ( net_aps_213246 ) , 
-    .z ( \TraceTargetNIU_Data[127] ) ) ;
-r0hd117_lnw3_tsmc_bufft04p00 compile_PI_7559_229128 ( .a ( net_aps_213247 ) , 
-    .z ( \TraceTargetNIU_Data[126] ) ) ;
-r0hd117_lnw3_tsmc_bufft04p00 compile_PI_7560_229129 ( .a ( net_aps_213248 ) , 
-    .z ( \TraceTargetNIU_Data[125] ) ) ;
-r0hd286_lnw3_tsmc_buft03p00 compile_PI_7561_229130 ( .a ( net_aps_213249 ) , 
-    .z ( \TraceTargetNIU_Data[124] ) ) ;
-r0hd286_lnw3_tsmc_buft03p00 compile_PI_7562_229131 ( .a ( net_aps_213250 ) , 
-    .z ( \TraceTargetNIU_Data[123] ) ) ;
+    .\TraceTargetNIU_Data[97] ( \TraceTargetNIU_Data_0[97] ) , 
+    .\TraceTargetNIU_Data[98] ( \TraceTargetNIU_Data_0[98] ) , 
+    .\TraceTargetNIU_Data[99] ( \TraceTargetNIU_Data_0[99] ) , 
+    .\TraceTargetNIU_Data[9] ( \TraceTargetNIU_Data_0[9] ) , 
+
