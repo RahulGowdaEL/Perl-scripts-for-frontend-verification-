d49fe0b3f4f1e1ff67cb333f5efdd3cca2b510e6
Rahul Gowda <70683277+RahulGowdaEL@users.noreply.github.com>
2025-01-03 11:56:36 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,33 +1,42 @@
-#!/usr/bin/perl
 use strict;
 use warnings;
 
-# File paths
-my $file1 = 'file1.txt';
-my $file2 = 'file2.txt';
-my $output_file = 'difference.txt';
+# Prompt user for input file, exclusion file, and output file
+print "Enter the input file name: ";
+chomp(my $input_file = <STDIN>);
 
-# Read keywords from file1 into a hash
-open(my $fh1, '<', $file1) or die "Cannot open $file1: $!";
-my %file1_keywords;
-while (my $line = <$fh1>) {
-    chomp $line;
-    $file1_keywords{$line} = 1;
+print "Enter the exclusion file name: ";
+chomp(my $exclusion_file = <STDIN>);
+
+print "Enter the output file name: ";
+chomp(my $output_file = <STDIN>);
+
+# Read exclusion file and build the exclusion hash
+open my $fh_exclusion, '<', $exclusion_file or die "Cannot open exclusion file: $!";
+my %exclusions;
+while (my $line = <$fh_exclusion>) {
+    chomp($line);
+    $line =~ s/^\s+|\s+$//g;  # Trim whitespace
+    $exclusions{$line} = 1 if $line;  # Store the signal names in a hash
 }
-close $fh1;
+close $fh_exclusion;
 
-# Read file2 and check for keywords not in file1
-open(my $fh2, '<', $file2) or die "Cannot open $file2: $!";
-open(my $out_fh, '>', $output_file) or die "Cannot open $output_file: $!";
+# Process the input file and write the output
+open my $fh_in, '<', $input_file or die "Cannot open input file: $!";
+open my $fh_out, '>', $output_file or die "Cannot open output file: $!";
 
-while (my $line = <$fh2>) {
-    chomp $line;
-    # Print keywords present in file2 but not in file1
-    if (!exists $file1_keywords{$line}) {
-        print $out_fh "$line\n";
+while (my $line = <$fh_in>) {
+    if ($line =~ /\\([\w\[\]\*]+)/) {  # Match lines with a backslash and potential signal name
+        my $signal_name = $1;  # Extract the signal name
+        if (exists $exclusions{$signal_name}) {
+            # Exact match found in exclusions, remove backslash
+            $line =~ s/\\($signal_name)/$1/g;
+        }
     }
+    print $fh_out $line;  # Write the (possibly modified) line to output
 }
-close $fh2;
-close $out_fh;
 
-print "Processing complete. Differences written to $output_file\n";
+close $fh_in;
+close $fh_out;
+
+print "Processing completed. Output written to $output_file.\n";
