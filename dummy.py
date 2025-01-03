5b99ae38778c0fa5d05d8ec745190676c68faaa6
Rahul Gowda <70683277+RahulGowdaEL@users.noreply.github.com>
2025-01-03 12:53:08 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -6,9 +6,14 @@ my $input_file     = 'input.v';       # Replace with your input file name
 my $exclusion_file = 'exclusions.txt'; # Replace with your exclusion file name
 my $output_file    = 'output.v';       # Replace with your desired output file name
 
-# Read exclusion file and build the exclusion list
+# Read the exclusion file and store exclusions in a hash
 open my $fh_exclusion, '<', $exclusion_file or die "Cannot open exclusion file: $!";
-my @exclusion_keywords = map { chomp; $_ } <$fh_exclusion>;
+my %exclusions;
+while (my $line = <$fh_exclusion>) {
+    chomp($line);
+    $line =~ s/^\s+|\s+$//g;  # Trim leading/trailing whitespace
+    $exclusions{$line} = 1 if $line;  # Store valid exclusion signals in the hash
+}
 close $fh_exclusion;
 
 # Process the input file
@@ -16,24 +21,17 @@ open my $fh_in, '<', $input_file or die "Cannot open input file: $!";
 open my $fh_out, '>', $output_file or die "Cannot open output file: $!";
 
 while (my $line = <$fh_in>) {
-    my $modified = $line;
-
-    foreach my $keyword (@exclusion_keywords) {
-        # Match and process set 1: Remove '\' for `.z (\keyword[...] )`
-        if ($modified =~ /\.z\s*\(\\$keyword\[(\d+)\]/) {
-            $modified =~ s/\.z\s*\(\\($keyword\[\d+\])/\( $1/g;
-        }
-
-        # Do not modify set 2: Lines containing `.\keyword[...]`
-        elsif ($modified =~ /\\$keyword_/) {
-            next; # Skip any modifications for set 2
+    # Check for signals in exclusion list and modify only those lines
+    foreach my $signal (keys %exclusions) {
+        if ($line =~ /\b\\$signal\b/) { # Match exact signal with backslash
+            $line =~ s/\\($signal)/$1/g; # Remove backslash for the matched signal
+            last; # Stop processing further exclusions for this line
         }
     }
-
-    print $fh_out $modified;
+    print $fh_out $line; # Write the processed or unmodified line to the output
 }
 
 close $fh_in;
 close $fh_out;
 
-print "Processing completed. Output written to $output_file.\n";
+print "Processing complete. Output written to $output_file.\n";
