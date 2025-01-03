baaf182860c9c56d866d5d97e73f500746807e97
Rahul Gowda <70683277+RahulGowdaEL@users.noreply.github.com>
2025-01-03 18:56:19 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -2,12 +2,12 @@
 use strict;
 use warnings;
 
-
+# File definitions
 my $input_file     = 'hm_dch_tile_top.v';       
 my $exclusion_file = 'port_name_1'; 
-my $output_file = "processed_output.v";
+my $output_file    = "processed_output.v";
 
-
+# Load exclusion signals
 open my $excl_fh, '<', $exclusion_file or die "Could not open exclusion file '$exclusion_file': $!\n";
 my %exclusion_signals;
 while (my $line = <$excl_fh>) {
@@ -16,15 +16,17 @@ while (my $line = <$excl_fh>) {
 }
 close $excl_fh;
 
-
+# Process input file
 open my $in_fh,  '<', $input_file  or die "Could not open input file '$input_file': $!\n";
 open my $out_fh, '>', $output_file or die "Could not open output file '$output_file': $!\n";
 
 while (my $line = <$in_fh>) {
-    
     foreach my $signal (keys %exclusion_signals) {
-        
+        # Handle standard case: Remove backslash for exclusion signals
         $line =~ s/\\($signal\[\d+\])/$1/g;
+        
+        # Handle the special case: Don't modify backslashes within complex parentheses
+        $line =~ s/(\\\.$signal\[\d+\]\s*\(\s*.*?\\$signal\[\d+\].*?\))/$1/g;
     }
     print $out_fh $line;
 }
