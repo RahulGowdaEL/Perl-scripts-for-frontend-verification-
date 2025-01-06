7f3ffc3673db6e6f59b9d9775e0bf849b8208955
Rahul Gowda <70683277+RahulGowdaEL@users.noreply.github.com>
2025-01-06 23:03:04 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -1,3 +1,50 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $input_file     = 'hm_dch_tile_top.v';       
+my $exclusion_file = 'port_name_1'; 
+my $output_file    = "processed_output.v";
+
+# Load exclusion signals
+open my $excl_fh, '<', $exclusion_file or die "Could not open exclusion file '$exclusion_file': $!\n";
+my %exclusion_signals;
+while (my $line = <$excl_fh>) {
+    chomp $line;
+    $exclusion_signals{$line} = 1;
+}
+close $excl_fh;
+
+# Process the input file
+open my $in_fh,  '<', $input_file  or die "Could not open input file '$input_file': $!\n";
+open my $out_fh, '>', $output_file or die "Could not open output file '$output_file': $!\n";
+
+while (my $line = <$in_fh>) {
+    # Preserve lines with leading spaces and matching `.\\signal_name[*]`
+    if ($line =~ /^\s*\.\\(\w+)\[\d+\]/) {
+        my $signal = $1;
+        if (exists $exclusion_signals{$signal}) {
+            print $out_fh $line;
+            next;
+        }
+    }
+
+    # Remove backslash for exclusion signals in signal lists
+    foreach my $signal (keys %exclusion_signals) {
+        # Case 1: Match `.\\signal_name[*]`
+        $line =~ s/\\($signal\[\d+\])/$1/g;
+        
+        # Case 2: Match `(\ \signal_name[*])` inside parentheses
+        $line =~ s/\( \\\($signal\[\d+\]\)/\( $1\)/g;
+    }
+
+    print $out_fh $line;
+}
+
+close $in_fh;
+close $out_fh;
+
+print "Processing complete. Output written to '$output_file'.\n";
 
 import gzip
 import os
