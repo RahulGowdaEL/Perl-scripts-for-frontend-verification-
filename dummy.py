ca428e6fcec5d3b5848594d357c1cfcc1089ce2a
Rahul Gowda <70683277+RahulGowdaEL@users.noreply.github.com>
2025-01-20 11:18:56 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -76,21 +76,3 @@ foreach my $entry (@log_entries) {
 
 close $out_fh;
 print "Comparison complete. Results saved in $output_file\n";
-
-
-
-UVM_INFO @ 5963332138 ps: uvm_test_top [uvm_test_top]  Dhyan print for time_period in ps is = 826 ps of chk_shub_clk
-UVM_INFO @ 5963332138 ps: uvm_test_top [uvm_test_top]  Dhyan print for freq in MHz is = 1210.000000 of chk_shub_clk 
-UVM_INFO @ 5963343928 ps: uvm_test_top [uvm_test_top]  Dhyan print for time_period in ps is = 4168 ps of chk_pcie_clk
-UVM_INFO @ 5963343928 ps: uvm_test_top [uvm_test_top]  Dhyan print for freq in MHz is = 239.000000 of chk_pcie_clk 
-UVM_INFO @ 5963378154 ps: uvm_test_top [uvm_test_top]  Dhyan print for time_period in ps is = 13334 ps of chk_cnoc_clk
-UVM_INFO @ 5963378154 ps: uvm_test_top [uvm_test_top]  Dhyan print for freq in MHz is = 74.000000 of chk_cnoc_clk 
-
-UVM_INFO @ 0.000ps: reporter@@shrm_rpmh_sequence []  FREQ_SWITCH is = LOWSVS_547_GCC
-from csv
-Name,Type,Domain,Functional Group,Generator,Max LOWSVS_D1,Max LOWSVS,Max SVS,Max SVS_L1,Max NOM,Max TURBO,Max TURBO_L1,SW Configurations,Access
-shub_clk,ext,ddrss_test_grp,ddrss_test,ext,250,250,333.333333,400,400,400,400,,
-pcie_clk,ext,ddrss_test_grp,ddrss_test,ext,250,250,333.333333,400,400,400,400,,
-cnoc_clk,cbc,ddrss_test_grp:div,ddrss_test_mux,sm_cbc,250,250,333.333333,400,400,400,400,,
-
-for shub_clk : freq_name = lowsvs freq_number from csv -> 250 (so if it's +or - 1) in output it should be showed as matched with all those parameters.
