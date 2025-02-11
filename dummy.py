fcfc93a7b5707208cce55ae8e360f0c189cd7a53
Rahul Gowda <70683277+RahulGowdaEL@users.noreply.github.com>
2025-02-11 14:57:17 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -2,6 +2,44 @@
 use strict;
 use warnings;
 
+# Input and output file
+my $input_file  = "power_log.txt";   # Change as needed
+my $output_file = "power_transitions.log";
+
+# Hash to store state transitions for each power rail
+my %power_transitions;
+
+# Open input log file
+open my $fh, '<', $input_file or die "Cannot open $input_file: $!";
+while (<$fh>) {
+    if (/LP_SS_STATE_CHANGE.*Supply Set '(.*?)' transitioned to state '(.*?)'/) {
+        my ($rail, $state) = ($1, $2);
+
+        # Normalize rail name to remove full hierarchy
+        $rail =~ s{.*/}{};  # Keep only last part
+
+        # Track state transitions
+        push @{ $power_transitions{$rail} }, $state 
+            if !@{ $power_transitions{$rail} } || $power_transitions{$rail}[-1] ne $state;
+    }
+}
+close $fh;
+
+# Write output file
+open my $out, '>', $output_file or die "Cannot write to $output_file: $!";
+foreach my $rail (sort keys %power_transitions) {
+    print $out "$rail: " . join(" --> ", @{ $power_transitions{$rail} }) . "\n";
+}
+close $out;
+
+print "Power transitions saved to $output_file\n";
+
+
+
+#!/usr/bin/perl
+use strict;
+use warnings;
+
 my $input_file = 'vcs_lpmsg.log';   
 my $output_file = 'voltage_log.txt';
 
