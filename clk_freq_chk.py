c8328d7b1c208b387ec2f0dc02518c4232f3d799
Rahul Gowda <70683277+RahulGowdaEL@users.noreply.github.com>
2025-08-10 18:38:20 +0530

    Update clk_freq_chk.py

--- clk_freq_chk.py
+++ clk_freq_chk.py
@@ -1,1 +1,20 @@
+clk_name : dpcc_ch3_mach9_atb_clk , at the time : 13836.673ns
+clk_name : dpcc_ch3_mach9_atb_clk, having frequency 0.000000
+clk_name : dpcc_ch3_mach9_atb_clk , at the time : 13845.009ns
+clk_name : dpcc_ch3_mach9_atb_clk, having frequency 240.000000
+clk_name : dpcc_ch3_mach9_atb_clk , at the time : 4132529.233ns
+clk_name : dpcc_ch3_mach9_atb_clk, having frequency 0.000000
+clk_name : dpcc_ch3_mach9_atb_clk , at the time : 4132537.569ns
+clk_name : dpcc_ch3_mach9_atb_clk, having frequency 240.000000
+clk_name : dpcc_ch3_mach9_atb_clk , at the time : 4349550.740ns
+clk_name : dpcc_ch3_mach9_atb_clk, having frequency 160.000000
+clk_name : dpcc_ch3_mach9_atb_clk , at the time : 4349567.408ns
+clk_name : dpcc_ch3_mach9_atb_clk, having frequency 120.000000
+clk_name : dpcc_ch3_mach9_atb_clk , at the time : 4503479.720ns
+clk_name : dpcc_ch3_mach9_atb_clk, having frequency 0.000000
+clk_name : dpcc_ch3_mach9_atb_clk , at the time : 4503496.388ns
+clk_name : dpcc_ch3_mach9_atb_clk, having frequency 120.000000
 
+Name	Type	Domain	Functional Group	Generator	Max LOWSVS	Max SVS	Max SVS_L1	Max NOM	Max TURBO	Max TURBO_L1	Max TURBO_L3	Max TURBO_L4
+dpcc_ch3_mach9_apb_clk	cbc	gcc_east_apb_grp	shub	sm_cbc	19.2	19.2	19.2	19.2	19.2	19.2	19.2	19.2
+dpcc_ch3_mach9_atb_clk	cbc	gcc_ddrss_atb_grp	shub	sm_cbc	60	120	120	240	300	300	300	300
